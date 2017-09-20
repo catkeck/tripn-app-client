b4e364a5836e10a4fda9b57d49255d999428a877
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-20 18:14:28 -0400

    made code for itinerary easier to work with by saving businesses and restaurants json data as arrays in preparation for filtering these arrays

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -14,14 +14,14 @@ class Itinerary extends React.Component {
 
   componentDidMount() {
     const adapter = new TripAdapter();
-    adapter.getActivities(this.props.data.match.params.location).then(json => this.setState({activities: json}))
-    adapter.getRestaurants(this.props.data.match.params.location).then(json => this.setState({restaurants: json}))
+    adapter.getActivities(this.props.data.match.params.location).then(json => this.setState({activities: json.businesses.businesses}))
+    adapter.getRestaurants(this.props.data.match.params.location).then(json => this.setState({restaurants: json.restaurants.businesses}))
   }
 
   filterWeather() {
-    
+
   }
-  
+
   render() {
     console.log(this.props.data.match.params.location)
     console.log(this.state)
@@ -30,11 +30,11 @@ class Itinerary extends React.Component {
         <div id="left-half">
           <div className="itinerary">
             <h1> Itinerary </h1>
-            {this.state.activities.businesses ? this.state.activities.businesses.businesses.slice(0,4).map(business => <Activity data={business}/>) : null}
+            {this.state.activities.length > 0 ? this.state.activities.slice(0,4).map(business => <Activity data={business}/>) : null}
           </div>
           <div className="food">
             <h1> Food </h1>
-            {this.state.restaurants.restaurants ? this.state.restaurants.restaurants.businesses.slice(0,3).map(business => <Activity data={business}/>) : null}
+            {this.state.restaurants.length > 0 ? this.state.restaurants.slice(0,3).map(business => <Activity data={business}/>) : null}
           </div>
         </div>
         <div id="right-half">
