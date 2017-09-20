90b55e7b05641d2509508320f6705160152e00c2
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-20 19:57:58 -0400

    added ability to delete values from current array

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 class Activity extends React.Component {
 
+  handleClick = (event) => {
+    this.props.deleteActivity(this.props.name)
+  }
+
 
   render() {
+
+
     return(
-      <div><div className="list-images"><img src={this.props.data.image_url} alt=""/></div><h2>{this.props.data.name}</h2><p>{this.props.data.location.display_address}</p><p>{this.props.data.display_phone}</p><p>{this.props.price}</p></div>
+      <div><div className="list-images"><img src={this.props.data.image_url} alt=""/></div><h2>{this.props.data.name}</h2><p>{this.props.data.location.display_address}</p><p>{this.props.data.display_phone}</p><p>{this.props.price}</p><button onClick={this.handleClick}>Delete</button></div>
     )
   }
 
--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -7,8 +7,8 @@ class Itinerary extends React.Component {
   constructor() {
     super()
     this.state={
-      activities: {},
-      restaurants: {}
+      activities: [],
+      restaurants: []
     }
   }
 
@@ -22,6 +22,25 @@ class Itinerary extends React.Component {
 
   }
 
+  deleteActivity = (activity) => {
+    const filteredActivities = this.state.activities.filter((element) => {
+      return element.name !== activity
+    })
+    this.setState({
+      activities: filteredActivities
+    })
+  }
+
+  deleteRestaurant = (restaurant) => {
+    const filteredRestaurants = this.state.restaurants.filter((element) => {
+      return element.name !== restaurant
+    })
+    this.setState({
+      restaurants: filteredRestaurants
+    })
+  }
+
+
   render() {
     console.log(this.props.data.match.params.location)
     console.log(this.state)
@@ -30,11 +49,11 @@ class Itinerary extends React.Component {
         <div id="left-half">
           <div className="itinerary">
             <h1> Itinerary </h1>
-            {this.state.activities.length > 0 ? this.state.activities.slice(0,4).map(business => <Activity data={business}/>) : null}
+            {this.state.activities.length > 0 ? this.state.activities.slice(0,4).map((business,index) => <Activity deleteActivity={this.deleteActivity} key={index} name={business.name} data={business}/>) : null}
           </div>
           <div className="food">
             <h1> Food </h1>
-            {this.state.restaurants.length > 0 ? this.state.restaurants.slice(0,3).map(business => <Activity data={business}/>) : null}
+            {this.state.restaurants.length > 0 ? this.state.restaurants.slice(0,3).map((business,index) => <Activity deleteActivity={this.deleteRestaurant} key={index} name={business.name} data={business}/>) : null}
           </div>
         </div>
         <div id="right-half">
