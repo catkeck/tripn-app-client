67b41c99911cf74e0d89f2dffa48ecdfed1f59e4
Caitlin Keck <caitlin.keck@gmail.com>
2017-10-30 00:23:26 -0400

    turned weather links to https

--- src/actions/itinerary.js
+++ src/actions/itinerary.js
@@ -87,7 +87,7 @@ export function fetchRestaurants(location) {
 export function fetchStringWeather(location) {
   return function(dispatch) {
     dispatch({type: "FETCHING_WEATHER"})
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=600c24f64be0542ee3eea23ad03ee915`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=600c24f64be0542ee3eea23ad03ee915`)
     .then(response => response.json())
     .then(
       weather => {
@@ -103,7 +103,7 @@ export function fetchCoordinateWeather(location) {
   const latlon = location.split(",")
   return function(dispatch) {
     dispatch({type: "FETCHING_WEATHER"})
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latlon[0]}&lon=${latlon[1]}&units=imperial&APPID=600c24f64be0542ee3eea23ad03ee915`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latlon[0]}&lon=${latlon[1]}&units=imperial&APPID=600c24f64be0542ee3eea23ad03ee915`)
       .then(response => response.json())
       .then(weather => {
         dispatch({type: "FETCHED_WEATHER", payload: weather
