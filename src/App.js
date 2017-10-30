b5275cead423220622bebe5efb9cd173d2128703
Caitlin Keck <caitlin.keck@gmail.com>
2017-10-29 23:32:16 -0400

    trying to use allowcrossdomain

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ import Contact from './components/Static/Contact'
 import AuthUber from './components/Uber/AuthUber'
 require('dotenv').config()
 
+var allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 class App extends Component {
   render() {
     return (
